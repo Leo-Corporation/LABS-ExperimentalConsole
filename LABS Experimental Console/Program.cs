9f8ffb8c11e8296a006fabaee7e49ccf68a3d223
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-07-20 11:41:35 +0200

    Update Program.cs

--- LABS Experimental Console/Program.cs
+++ LABS Experimental Console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using LABS_Experimental_Console.Classes;
 using System;
+using System.Linq;
 
 namespace LABS_Experimental_Console
 {
@@ -24,15 +25,42 @@ namespace LABS_Experimental_Console
                         Functions.WriteHelp();
                         GoHome();
                         break;
+                    case "ver": // Si la commande est "help"
+                        Console.ForegroundColor = ConsoleColor.Blue; // Changer la couleur
+                        Console.WriteLine("Version {0}, Copyright {1} - Léo Corporation", Definitions.Version, DateTime.Now.Year); // Ecrire
+                        GoHome();
+                        break;
+                    case "about": // Si la commande est "help"
+                        Console.ForegroundColor = ConsoleColor.Blue; // Changer la couleur
+                        Console.WriteLine("LABS Experimental Console © {0}", DateTime.Now.Year) ; // Ecrire
+                        GoHome();
+                        break;
+                    case "cls":
+                        Console.Clear(); // Effacer
+                        GoHome();
+                        break;
+                    case "clear":
+                        Console.Clear(); // Effacer
+                        GoHome();
+                        break;
+                    case "list":
+                        Functions.WriteCommands();
+                        GoHome();
+                        break;
+                    case "update":
+
+                        GoHome();
+                        break;
                 }
             }
         }
 
         public void GoHome()
         {
-            Console.ResetColor(); // Metttre la couleur par défaut
+            Console.ForegroundColor = ConsoleColor.Yellow; // Changer la couleur
             Console.WriteLine(""); // Ecrire
             Console.WriteLine("Pour commencer, tapez une commande. Tapez 'help' pour afficher l'aide."); // Ecrire
+            Console.ResetColor(); // Mettre la couleur par défaut
             ExecuteCommand(Console.ReadLine());
         }
 
@@ -41,21 +69,19 @@ namespace LABS_Experimental_Console
             Console.ForegroundColor = ConsoleColor.Red; // Mettre en rouge
             Console.WriteLine("La commande que vous avez executée n'existe pas."); // Afficher le message
             Console.ResetColor(); // Mettre la couleur par défaut
+            GoHome();
         }
 
         public bool IsCommandExist(string command)
         {
-            bool res = false; // Résultat
-            foreach (string com in Definitions.Commands)
+            bool res = true; // Résultat
+            if (Definitions.Commands.Contains(command))
             {
-                if (com == command) // Si la commande existe
-                {
-                    res = true;
-                }
-                else // Si la commande n'existe pas
-                {
-                    res = false;
-                }
+                res = true;
+            }
+            else
+            {
+                res = false;
             }
             return res;
         }
