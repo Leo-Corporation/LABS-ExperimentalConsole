7d3b0ec96562e856f813b0e4718be1191d3f5661
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-07-20 10:23:23 +0200

    Ajout de LABS Experimental Console (.NET Core)

---
+++ LABS Experimental Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LABS_Experimental_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
