3256a0827540178f824360334030def8d2760e83
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-07-20 11:09:41 +0200

    Update Program.cs

--- LABS Experimental Console/Program.cs
+++ LABS Experimental Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LABS_Experimental_Console.Classes;
+using System;
 
 namespace LABS_Experimental_Console
 {
@@ -6,7 +7,57 @@ namespace LABS_Experimental_Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new Program().GoHome();
+        }
+
+        public void ExecuteCommand(string command)
+        {
+            if (!IsCommandExist(command))
+            {
+                WriteWrongCommandMessage();
+            }
+            else
+            {
+                switch (command.ToLower())
+                {
+                    case "help": // Si la commande est "help"
+                        Functions.WriteHelp();
+                        GoHome();
+                        break;
+                }
+            }
+        }
+
+        public void GoHome()
+        {
+            Console.ResetColor(); // Metttre la couleur par défaut
+            Console.WriteLine(""); // Ecrire
+            Console.WriteLine("Pour commencer, tapez une commande. Tapez 'help' pour afficher l'aide."); // Ecrire
+            ExecuteCommand(Console.ReadLine());
+        }
+
+        public void WriteWrongCommandMessage()
+        {
+            Console.ForegroundColor = ConsoleColor.Red; // Mettre en rouge
+            Console.WriteLine("La commande que vous avez executée n'existe pas."); // Afficher le message
+            Console.ResetColor(); // Mettre la couleur par défaut
+        }
+
+        public bool IsCommandExist(string command)
+        {
+            bool res = false; // Résultat
+            foreach (string com in Definitions.Commands)
+            {
+                if (com == command) // Si la commande existe
+                {
+                    res = true;
+                }
+                else // Si la commande n'existe pas
+                {
+                    res = false;
+                }
+            }
+            return res;
         }
     }
 }
