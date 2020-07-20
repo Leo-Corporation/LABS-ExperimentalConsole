45b7a29b086cd3dcfc7b0038640abd92c9c43d35
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-07-20 10:30:02 +0200

    Create Definitions.cs

---
+++ LABS Experimental Console/Classes/Definitions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LABS_Experimental_Console.Classes
+{
+    public static class Definitions
+    {
+        public static readonly string Version = "1.0.0.2007"; // Version de la console
+        public static readonly string[] Commands = {"help", "ver", "update", "exit", "about", "clear", "cl"}; // Commandes
+    }
+}
