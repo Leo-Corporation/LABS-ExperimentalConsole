cc8adddae95028da7ae263ae16cff8c557aa37e9
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-07-20 11:41:23 +0200

    Update Definitions.cs

--- LABS Experimental Console/Classes/Definitions.cs
+++ LABS Experimental Console/Classes/Definitions.cs
@@ -7,6 +7,6 @@ namespace LABS_Experimental_Console.Classes
     public static class Definitions
     {
         public static readonly string Version = "1.0.0.2007"; // Version de la console
-        public static readonly string[] Commands = { "help", "ver", "update", "exit", "about", "clear", "cl" }; // Commandes
+        public static readonly string[] Commands = { "help", "ver", "update", "list", "exit", "about", "clear", "cls" }; // Commandes
     }
 }
