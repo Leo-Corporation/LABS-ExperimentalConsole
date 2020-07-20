584ce746a9d354ed9975efcd905801170d84da25
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-07-20 11:41:30 +0200

    Update Functions.cs

--- LABS Experimental Console/Classes/Functions.cs
+++ LABS Experimental Console/Classes/Functions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Net;
 using System.Text;
 
 namespace LABS_Experimental_Console.Classes
@@ -8,17 +10,44 @@ namespace LABS_Experimental_Console.Classes
     {
         public static void WriteHelp()
         {
-            Console.ForegroundColor = ConsoleColor.Magenta; // Changer la couleur
-            Console.WriteLine("*----* Aide *----*"); // Ecrire
+            Console.ForegroundColor = ConsoleColor.Blue; // Changer la couleur
+            Console.WriteLine("+----+ Aide +----+"); // Ecrire
             Console.ResetColor(); // Mettre la couleur par défaut
             Console.WriteLine("Voici les diverses commandes disponibles :"); // Ecrire
             Console.WriteLine(""); // Ecrire
             Console.WriteLine("about : Affiche les crédits"); // Ecrire
-            Console.WriteLine("cl, clear : Efface la console"); // Ecrire
+            Console.WriteLine("clear : Efface la console"); // Ecrire
             Console.WriteLine("exit : Ferme la console"); // Ecrire
             Console.WriteLine("help : Affiche l'aide"); // Ecrire
             Console.WriteLine("update : Vérifie si des mises à jour sont disponibles"); // Ecrire
             Console.WriteLine("ver : Affiche la version"); // Ecrire
+        }
+
+        public static void WriteCommands() 
+        {
+            Console.ForegroundColor = ConsoleColor.Blue; // Changer la couleur
+            Console.WriteLine("+----+ Liste des commandes +----+"); // Ecrire
+            Console.ResetColor(); // Mettre la couleur par défaut
+            Console.WriteLine("Voici la liste des commandes"); // Ecrire
+            Console.WriteLine(""); // Ecrire
+            for (int i = 0; i < Definitions.Commands.Length; i++)
+            {
+                Console.WriteLine(Definitions.Commands[i]); // Ecrire
+            }
+        }
+
+        public static bool IsUpdateAvailable()
+        {
+            bool res = false; // Résultat
+            if (Definitions.Version == new WebClient().DownloadString(""))
+            {
+                res = true;   
+            }
+            else
+            {
+                res = false;
+            }
+            return res;
         }
     }
 }
